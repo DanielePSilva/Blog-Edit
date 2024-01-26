5e2431dff33642d28f28bf63f185e42b645caac6
Daniele Silva <danielepereira9503@gmail.com>
2024-01-26 17:41:55 +0000

    card component

---
+++ blog-edit/src/components/postCard/index.tsx
@@ -0,0 +1,31 @@
+import { Card, CardBody, CardText, CardTitle } from "reactstrap";
+
+//postCard Components
+
+interface CardProps {
+  imgUrl: string;
+  title: string;
+  description: string;
+}
+
+export function CardComponent({ title, description, imgUrl }: CardProps) {
+  return (
+    <Card
+      style={{
+        width: "18rem",
+        padding: "15px",
+      }}
+    >
+      <img
+        alt="Sample"
+        src={imgUrl}
+        style={{ borderRadius: "10px", height: "rem" }}
+      />
+
+      <CardBody>
+        <CardTitle tag="h5">{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+    </Card>
+  );
+}
